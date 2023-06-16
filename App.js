3f8b80a7cebbae70a36cb5a58579d1a4e341b4af
Imran <imranjaleel2331@gmail.com>
2023-06-16 23:21:44 +0530

    new start

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import {  View,StyleSheet } from "react-native";
-import BgImage from "./Screens/BgImage";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <BgImage/>
       
     </View>
   );
@@ -13,6 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'black'
+    backgroundColor:'black',
+    
   }
 });
